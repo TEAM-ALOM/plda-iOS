6b25f61c02740218650b3d113ed31cb107273ebe
YugyeongChoi <xyukyeong@naver.com>
2023-07-13 03:03:53 +0900

    메인화면 단순화 구현

---
+++ Plda/PldaApp.swift
@@ -0,0 +1,18 @@
+//
+//  PldaApp.swift
+//  Plda
+//
+//  Created by 최유경 on 2023/07/07.
+//
+
+import SwiftUI
+
+@main
+struct PldaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+    
+}
